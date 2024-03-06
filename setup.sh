f672df4395512c7c59f9a44f2e231210cf411a9c
Yassir Sbai <yassirsbai04@gmail.com>
2024-03-06 13:09:13 +0100

    .

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ alias_command="alias storage='du -sh /nfs/sgoinfre/goinfre/Perso/$USER'"
 shell=${SHELL##*/}
 
 echo "$alias_command" >> "$HOME/.${shell}rc"
-
+echo "Loading..."
+sleep 2
 echo -e "\e[32mnow you can type \"storage\" to see your sgoinfre used capacity\e[0m"
 echo -e "\e[32mEnjoy !!!!\e[0m"
 
